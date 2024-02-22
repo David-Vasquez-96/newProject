c3e70e237494c6abac2162c7c544f7540724e4fe
Mario David Herrera Vásquez <mdherrerav@tse.org.gt>
2024-02-21 18:39:22 -0600

    se agrego una estructura de menu desplegable

--- src/page/Home/Body/AppBarPrincipal/MenuDesplegable/index.js
+++ src/page/Home/Body/AppBarPrincipal/MenuDesplegable/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-import {SwipeableDrawer, Divider, List, ListItem, ListItemIcon, ListItemText, ListSubheader} from '@material-ui/core';
-import { Description, People } from '@material-ui/icons';
+import {SwipeableDrawer, Divider, List, ListItem, ListItemIcon, ListItemText, ListSubheader, Collapse} from '@material-ui/core';
+import { Description, ExpandLess, ExpandMore, People } from '@material-ui/icons';
 import {Link } from 'react-router-dom';
 // REDUX **************************
 import { useSelector, useDispatch} from 'react-redux';
@@ -21,13 +21,19 @@ export default function SwipeableTemporaryDrawer(props) {
     const classes = useStyles();
     const dispatch = useDispatch();
     const menuList = useSelector( state => state.security.menu); 
+    const [open, setOpen] = React.useState({});
+
+    const handleClick = (index) => {
+        setOpen({ ...open, [index]: !open[index],});
+    };
 
     const FuncionGuardarMenu = () => {
         dispatch(setMenu([]))
         const menu = [
-            {title: 'Categorias', icon: <Description />, to: '/categorias'},
-            {title: 'Procesos', icon: <Description />, to: '/procesos'},
-            {title: 'Usuarios', icon: <People />, to: '/usuarios'},
+            {title: 'Módulo de Categorias', icon: <Description />, to: '/categorias'},
+            {title: 'Módulo de Procesos', icon: <Description />, to: '/procesos'},
+            {title: 'Módulo de Usuarios', icon: <People />, to: '/usuarios'},
+            {title: 'Módulo de Menús', icon: <People />, to: '/usuarios'},
         ]
         dispatch(setMenu(menu))
     }
@@ -45,19 +51,34 @@ export default function SwipeableTemporaryDrawer(props) {
                 }            
             >
                 <Divider />
-                {
+                {/* {
                     menuList.map((label, index)=>(                        
                         <ListItem button to={label?.to} component={Link} key={index}>
                             <ListItemIcon>{label?.icon} </ListItemIcon>
                             <ListItemText primary={label?.title} />
                         </ListItem>
                     ))
+                } */}
+                {
+                    [0, 1, 2].map((index) => (
+                        <React.Fragment key={index}>
+                            <ListItem button onClick={() => handleClick(index)}>
+                                <ListItemText primary={`Nested List Group ${index + 1}`} />
+                                {open[index] ? <ExpandLess /> : <ExpandMore />}
+                            </ListItem>
+                            <Collapse in={open[index]} timeout="auto" unmountOnExit>
+                                <List component="div" disablePadding>
+                                    <ListItem button className={classes.nested}>
+                                        <ListItemText primary="Item 1" />
+                                    </ListItem>
+                                    <ListItem button className={classes.nested}>
+                                        <ListItemText primary="Item 2" />
+                                    </ListItem>
+                                </List>
+                            </Collapse>
+                        </React.Fragment>
+                    ))
                 }
-                {/* <Divider /> */}
-                {/* <ListItem button onClick={props.onLogout}>
-                    <ListItemIcon> <ExitToApp /></ListItemIcon>
-                    <ListItemText primary="Cerrar sesión" />
-                </ListItem> */}
             </List>
         </div>
     );
@@ -71,8 +92,8 @@ return (
             <SwipeableDrawer
                 anchor={props.menuDirection}
                 open={props.openMenu}
-                onClose={props.FuncionCerrarOpenMenu}
-                onOpen={(props.menuDirection, true)}
+                // onClose={props.FuncionCerrarOpenMenu}
+                // onOpen={(props.menuDirection, true)}
             >        
                 {list(props.menuDirection)}
             </SwipeableDrawer>
